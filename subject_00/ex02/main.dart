cf5cbfeb3d93e37cf4fe22955708724fee207fee
suhwan22 <tnghkswkd123@naver.com>
2022-08-19 12:13:15 +0900

    subject_00 / ex02

---
+++ subject_00/ex02/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'widget_lifecycle',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'Stateless? or Stateful?'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: Text(widget.title),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    count = 41;
+  }
+
+  void _incrementCount() {
+    setState(() {
+      count++;
+    });
+  }
+
+  Widget _buildBody() {
+    return Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(
+          '$count',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        TextButton(
+          onPressed: () {
+            _incrementCount();
+            print(count);
+            if (count == 42) {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text('climbing lover')));
+            }
+          },
+          style: TextButton.styleFrom(
+            primary: Colors.white,
+            backgroundColor: Colors.green,
+          ),
+          child: const Text('Add'),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+}
